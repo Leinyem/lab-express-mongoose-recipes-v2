0b4ce62e483b34c51516622097ec03b3b9131326
Leinyem <alfonmeyniel@gmail.com>
2025-04-02 22:18:30 +0200

    partially done

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model");
 
 const app = express();
 
@@ -11,15 +13,34 @@ app.use(express.json());
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
 
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 
 // ROUTES
-//  GET  / route - This is just an example route
-app.get('/', (req, res) => {
-    res.send("<h1>LAB | Express Mongoose Recipes</h1>");
-});
 
+app.post("/recipes", (req, res)=>{
+
+Recipe.create({
+
+title: req.body.title,
+instruccions: req.body.instructions,
+level: req.body.level,
+ingredients: req.body.ingredients,
+image: req.body.image,
+duration: req.body.duration,
+isArchived: req.body.isArchived,
+created: req.body.created,
+
+})
+
+.then((createdRecipe))
+
+})
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
